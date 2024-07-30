8388a7dd09ed1ab2f8829d659d71559086830ca9
cubewhy <cubewhy@163.com>
2024-07-30 19:00:38 +0800

    extra args

--- src/main/java/org/cubewhy/api/controller/LauncherController.java
+++ src/main/java/org/cubewhy/api/controller/LauncherController.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import org.cubewhy.api.entity.GameArtifactInfo;
 import org.cubewhy.api.entity.LaunchRequest;
 import org.cubewhy.api.entity.LaunchTypeData;
+import org.cubewhy.api.entity.RuntimeInfo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,8 @@ public class LauncherController {
     public GameArtifactInfo launch(@NotNull HttpServletRequest request) throws Exception {
         LaunchRequest launchRequest = new Gson().fromJson(new String(request.getInputStream().readAllBytes()), LaunchRequest.class);
         return GameArtifactInfo.builder()
+                .jre(RuntimeInfo.builder()
+                        .extraArguments(config.getExtraArguments()).build())
                 .launchTypeData(LaunchTypeData.builder()
                         .mainClass(config.getLunarMain())
                         .build())
--- src/main/java/org/cubewhy/api/entity/ServerConfig.java
+++ src/main/java/org/cubewhy/api/entity/ServerConfig.java
@@ -2,7 +2,11 @@ package org.cubewhy.api.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class ServerConfig {
     String lunarMain = "com.moonsworth.lunar.genesis.Genesis";
+    List<String> extraArguments = new ArrayList<>(List.of(new String[]{"--add-modules", "jdk.naming.dns", "--add-exports", "jdk.naming.dns/com.sun.jndi.dns=java.naming", "-Djna.boot.library.path=natives", "-Dlog4j2.formatMsgNoLookups=true", "--add-opens", "java.base/java.io=ALL-UNNAMED", "-XX:+UseStringDeduplication"}));
 }
