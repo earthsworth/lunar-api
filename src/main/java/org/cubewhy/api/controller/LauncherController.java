5424fc07be6a26bbafa29d072eacd961574d3f14
cubewhy <cubewhy@163.com>
2024-07-30 19:08:16 +0800

    todo

--- src/main/java/org/cubewhy/api/controller/LauncherController.java
+++ src/main/java/org/cubewhy/api/controller/LauncherController.java
@@ -3,13 +3,13 @@ package org.cubewhy.api.controller;
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
-import org.cubewhy.api.entity.GameArtifactInfo;
-import org.cubewhy.api.entity.LaunchRequest;
-import org.cubewhy.api.entity.LaunchTypeData;
-import org.cubewhy.api.entity.RuntimeInfo;
+import org.cubewhy.api.entity.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.cubewhy.api.LunarApiApplication.config;
 
@@ -19,11 +19,15 @@ public class LauncherController {
     @PostMapping("launch")
     public GameArtifactInfo launch(@NotNull HttpServletRequest request) throws Exception {
         LaunchRequest launchRequest = new Gson().fromJson(new String(request.getInputStream().readAllBytes()), LaunchRequest.class);
+        List<Artifact> artifacts = new ArrayList<>();
+        Textures textures = Textures.builder().build(); // todo parse textures from files
+        // todo find artifacts
         return GameArtifactInfo.builder()
-                .jre(RuntimeInfo.builder()
-                        .extraArguments(config.getExtraArguments()).build())
+                .jre(RuntimeInfo.builder().extraArguments(config.getExtraArguments()).build())
+                .textures(textures)
                 .launchTypeData(LaunchTypeData.builder()
                         .mainClass(config.getLunarMain())
+                        .artifacts(artifacts)
                         .build())
                 .build();
         // todo
