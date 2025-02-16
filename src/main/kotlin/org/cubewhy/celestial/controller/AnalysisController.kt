1e4ef38fba74bc675b7a2f083a95553af8bb5552
zszf114514 <2792668017@foxmail.com>
2025-02-16 11:09:34 +0800

    change analysis url

--- src/main/kotlin/org/cubewhy/celestial/controller/AnalysisController.kt
+++ src/main/kotlin/org/cubewhy/celestial/controller/AnalysisController.kt
@@ -4,18 +4,20 @@ import org.cubewhy.celestial.entity.Analysis
 import org.cubewhy.celestial.entity.RestBean
 import org.cubewhy.celestial.service.AnalysisService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
-@RestController("/api/admin")
+@RestController
+@RequestMapping("/api/analysis")
 class AnalysisController(
     private val analysisService: AnalysisService
 ) {
     /**
      * @param after timestamp, after this time's analysis
      */
-    @PostMapping("/analysis")
+    @PostMapping("/")
     suspend fun analysis(@RequestParam after: Long): RestBean<List<Analysis>> {
         return RestBean.success(analysisService.getAnalysisAfter(Instant.ofEpochSecond(after)))
     }
