f7b6d7ded862196c3e6cea1469a6c94949279fba
cubewhy <cubewhy@outlook.com>
2025-04-13 10:52:38 +0800

    feat(dashboard): automate the process of adding the token to the header

--- dashboard/src/api/axiosInstance.ts
+++ dashboard/src/api/axiosInstance.ts
@@ -1,12 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
+import { store } from "../store/store";
 
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: "/api",
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json',
-  },
+    "Content-Type": "application/json"
+  }
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = store.getState().auth.token;
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
   response => response,
