e7fedb3e7e1261330150b0df6b532f4bf633aec5
cubewhy <cubewhy@outlook.com>
2025-04-20 21:40:08 +0800

    style: remove unused inject

--- src/main/kotlin/org/cubewhy/celestial/entity/dto/EditRoleDTO.kt
+++
@@ -1,6 +0,0 @@
-package org.cubewhy.celestial.entity.dto
-
-data class EditRoleDTO(
-    val user: String,
-    val role: String
-)
--- src/main/kotlin/org/cubewhy/celestial/service/AdminService.kt
+++ src/main/kotlin/org/cubewhy/celestial/service/AdminService.kt
@@ -1,7 +1,5 @@
 package org.cubewhy.celestial.service
 
-import org.cubewhy.celestial.entity.dto.EditRoleDTO
-import org.cubewhy.celestial.entity.dto.TogglePlusDTO
 import org.cubewhy.celestial.entity.vo.PlayerInfoVO
 
 interface AdminService {
