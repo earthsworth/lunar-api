34f5d5dbdd3419d413d43ab1ac043e6e861986b2
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-06-25 21:43:57 +0800

    chore: add more logo colors

--- src/main/kotlin/org/cubewhy/celestial/entity/LogoColor.kt
+++ src/main/kotlin/org/cubewhy/celestial/entity/LogoColor.kt
@@ -1,6 +1,8 @@
 package org.cubewhy.celestial.entity
 
-enum class LogoColor(val color: Int) {
+enum class LogoColor(
+    val color: Int,
+) {
     BLACK(0x0),
     WHITE(0xFFFFFF),
     GREEN(0x3BBE54),
@@ -9,7 +11,12 @@ enum class LogoColor(val color: Int) {
     CYAN(0x00FFF0),
     YELLOW(0xEEC42A),
     ORANGE(0xE17E2E),
-    GOLD(0xFCAC04);
+    GOLD(0xFCAC04),
+    DEEP_CYAN(0x07688b),
+    SAGE_GREEN(0x749472),
+    SKY(0x4cadd0),
+    LIME(0x369876),
+    LIGHT_PURPLE(0x8579f1), ;
 
     companion object {
         fun findIgnoreCase(name: String): LogoColor? = entries.firstOrNull { it.name.equals(name, ignoreCase = true) }
--- src/main/kotlin/org/cubewhy/celestial/entity/Role.kt
+++ src/main/kotlin/org/cubewhy/celestial/entity/Role.kt
@@ -1,7 +1,9 @@
 package org.cubewhy.celestial.entity
 
 @Suppress("UNUSED")
-enum class Role(vararg val availableLogoColors: LogoColor) {
+enum class Role(
+    vararg val availableLogoColors: LogoColor,
+) {
     USER(LogoColor.WHITE),
     ADMIN(*LogoColor.entries.toTypedArray()),
     STAFF(LogoColor.GREEN),
@@ -10,5 +12,13 @@ enum class Role(vararg val availableLogoColors: LogoColor) {
     SPONSOR(LogoColor.ORANGE),
     PARTNER(LogoColor.GOLD),
 
-    LUNAR_CN_2K25(LogoColor.BLACK)
+    LUNAR_CN_2K25(LogoColor.BLACK),
+
+    MEDIA(LogoColor.SKY),
+    MEDIA_PLUS(LogoColor.LIGHT_PURPLE),
+
+    HELPER(LogoColor.SAGE_GREEN),
+    HELPER_PLUS(LogoColor.LIME),
+
+    TESTER(LogoColor.DEEP_CYAN),
 }
